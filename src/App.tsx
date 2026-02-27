e9a7c38b8afa77c550bf1fbba1a7564eb50eaf11
britto <jose@britto.tech>
2026-02-26 21:43:34 -0500

    add blade1, blade3 and remove ring2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import './App.css'
 import Search from "./components/Search.tsx";
 import BackgroundVideo from "./components/BackgroundVideo.tsx";
-import ring2 from './assets/ring2.webm';
 import blade2 from './assets/blade2.mp4';
+import blade3 from './assets/Blade3_1080p.mp4';
+import blade4 from './assets/Blade4_1080p.mp4';
 import {useState} from "react";
 
 function App() {
 
 
-    const videos = [ring2, blade2];
+    const videos = [blade2, blade3, blade4];
 
     const [currentVideo] = useState(() =>
         Math.floor(Math.random() * videos.length)
