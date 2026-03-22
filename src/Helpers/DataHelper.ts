a7ea26e30fa774356f89b39dce49250bd0427cf3
britto <jose@britto.tech>
2026-03-22 01:12:54 -0400

    Cache data for 30 days

--- src/Helpers/DataHelper.ts
+++ src/Helpers/DataHelper.ts
@@ -1,14 +1,67 @@
 import Papa from 'papaparse';
 
-const SHEETS_URL_START = "https://docs.google.com/spreadsheets/d/e/"
+const SHEETS_URL_START = "https://docs.google.com/spreadsheets/d/e/";
+const CACHE_EXPIRY_DAYS = 30;
+const TEXT_URL_LAST_REQUEST_TIME_KEY = "LAST_REQUEST_TIME_TEXT_URL";
+const RESOURCE_DATA_LAST_REQUEST_TIME_KEY = "LAST_REQUEST_TIME_RESOURCE_DATA";
+const TEXT_SETTINGS_LAST_REQUEST_TIME_KEY = "LAST_REQUEST_TIME_TEXT_SETTINGS";
+const DATA_CACHE_NAME = "data-cache-v1";
 
-export async function GetTextUrlDict(sheetId: string = "2PACX-1vScjvrdF1f9q1bM8WMFhohaOqAwudfNoyN4BCORkVM3nEcfEa4muQCqdC2u3XXNd-aqWoXywckEGBzm") {
+async function CacheResponse(localStorageKey: string, url: string, response: Response) {
+    const cache = await caches.open(DATA_CACHE_NAME);
+    await cache.put(url, response);
+    localStorage.setItem(localStorageKey, Date.now() + "");
+}
+
+
+async function GetCachedResponse<T>(localStorageKey: string, url: string, getDataOnlineFunction: () => Promise<T>): Promise<Response | null | undefined> {
+    const lastRequestTimeRaw = localStorage.getItem(localStorageKey);
+    let lastRequestTime = 0;
+    if(lastRequestTimeRaw && Number.isFinite(+lastRequestTimeRaw) && (+lastRequestTimeRaw) > 0) {
+        lastRequestTime = +lastRequestTimeRaw;
+    }
+    if(Date.now() - lastRequestTime < CACHE_EXPIRY_DAYS * 24 * 60 * 60 * 1000) {
+        try{
+            console.log(`Loading ${localStorageKey} from cache...`);
+            const cache = await caches.open(DATA_CACHE_NAME);
+            const response = cache.match(url);
+            if(response) {
+                console.log(localStorageKey + " loaded from cache.");
+                getDataOnlineFunction()
+                    .then(() => {
+                        console.log(localStorageKey + " downloaded and saved to cache.");
+                    });
+                return response;
+            }
+            console.log(`No cached ${localStorageKey} found.`);
+        } catch (error) {
+            console.error("Failed to load "+ localStorageKey + " from cache with error: ", JSON.stringify(error));
+        }
+        return null;
+    } else {
+        console.log(`${localStorageKey} is older than ${CACHE_EXPIRY_DAYS} days. Ignoring cache...`);
+    }
+}
+
+export async function GetTextUrlDict(sheetId: string = "2PACX-1vScjvrdF1f9q1bM8WMFhohaOqAwudfNoyN4BCORkVM3nEcfEa4muQCqdC2u3XXNd-aqWoXywckEGBzm", skipCache = false) {
     const url = SHEETS_URL_START + sheetId + "/pubhtml/sheet?headers=false&gid=0\n";
 
-    const response = await fetch(url);
+    let response: Response | null | undefined = null;
+    if(!skipCache) {
+        response = await GetCachedResponse(TEXT_URL_LAST_REQUEST_TIME_KEY, url, async () => {
+            return GetTextUrlDict(sheetId,true);
+        });
+    }
+
+    if(!response) {
+        response = await fetch(url);
+    }
     if (!response.ok) {
         throw new Error(response.statusText);
     }
+
+    await CacheResponse(TEXT_URL_LAST_REQUEST_TIME_KEY, url, response.clone()); //.clone() to remove error saying response is already consumed
+
     const html = await response.text();
     const doc = new DOMParser().parseFromString(html, "text/html");
     const dict: { [key: string]: string } = {};
@@ -30,13 +83,26 @@ export interface Site {
     link?: string;
 }
 
-export async function GetResourceData (sheetId: string = "2PACX-1vScjvrdF1f9q1bM8WMFhohaOqAwudfNoyN4BCORkVM3nEcfEa4muQCqdC2u3XXNd-aqWoXywckEGBzm") : Promise<Site[]> {
+export async function GetResourceData (sheetId: string = "2PACX-1vScjvrdF1f9q1bM8WMFhohaOqAwudfNoyN4BCORkVM3nEcfEa4muQCqdC2u3XXNd-aqWoXywckEGBzm", skipCache = false) : Promise<Site[]> {
     const url = SHEETS_URL_START + sheetId + "/pub?output=csv";
-    //const urlPromise = GetTextUrlDict(sheetId);
-    const response = await fetch(url);
+
+    let response: Response | null | undefined = null;
+    if(!skipCache) {
+        response = await GetCachedResponse(RESOURCE_DATA_LAST_REQUEST_TIME_KEY, url, async () => {
+            return GetResourceData(sheetId,true);
+        });
+    }
+
+    if(!response) {
+        response = await fetch(url);
+    }
+
     if (!response.ok) {
         throw new Error(response.statusText);
     }
+
+    await CacheResponse(RESOURCE_DATA_LAST_REQUEST_TIME_KEY, url, response.clone()); //.clone() to remove error saying response is already consumed
+
     const csv = await response.text();
     const data = Papa.parse<never>(csv, {
         header: true
@@ -67,12 +133,25 @@ export interface TextSettings {
     svg: string;
 }
 
-export async function GetTextSettingsData(sheetId: string = "2PACX-1vRUnyp44bSlnakCeyl0UuaN_PMvUa4SJ7Sj2ImjiyJYpLMSvIoLn7xOPSURSWJm-W-0q0iTSSaxYzVn"): Promise<Map<string, TextSettings>> {
+export async function GetTextSettingsData(sheetId: string = "2PACX-1vRUnyp44bSlnakCeyl0UuaN_PMvUa4SJ7Sj2ImjiyJYpLMSvIoLn7xOPSURSWJm-W-0q0iTSSaxYzVn", skipCache = false): Promise<Map<string, TextSettings>> {
     const url = SHEETS_URL_START + sheetId + "/pub?output=csv";
-    const response = await fetch(url);
+
+    let response: Response | null | undefined = null;
+    if(!skipCache) {
+        response = await GetCachedResponse(TEXT_SETTINGS_LAST_REQUEST_TIME_KEY, url, async () => {
+            return GetTextSettingsData(sheetId,true);
+        });
+    }
+
+    if(!response) {
+        response = await fetch(url);
+    }
     if (!response.ok) {
         throw new Error(response.statusText);
     }
+
+    await CacheResponse(TEXT_SETTINGS_LAST_REQUEST_TIME_KEY, url, response.clone()); //.clone() to remove error saying response is already consumed
+
     const csv = await response.text();
     const data = Papa.parse<never>(csv, {
         header: true
